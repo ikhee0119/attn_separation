76963439db7d217084210bd7cd07ea4c882a1e7c
ikhee0119 <ikhee0119@gmail.com>
2018-11-08 23:10:20 +0900

    context added and refactoring

--- models/Separator.py
+++ models/Separator.py
@@ -2,108 +2,101 @@
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as f
 
 
 class AttnNet(nn.Module):
-    def __init__(self, attention_fn, num_layers, num_filters, enc_filter_size, dec_filter_size):
+    def __init__(self, attention_fn, num_layers, num_filters, enc_filter_size, dec_filter_size,
+                 is_skip_attention=True, is_diff=True, context=False):
         super(AttnNet, self).__init__()
 
-        self.F = F(enc_filter_size, num_filters)
+        self.is_skip_attention = is_skip_attention
+        self.is_diff = is_diff
+        self.context = context
 
-        self.conv1 = conv(enc_filter_size, num_filters*8, num_filters*9)
+        self.F = F(enc_filter_size, num_filters, num_layers, context)
+        self.conv1 = conv(enc_filter_size, num_filters*num_layers, num_filters*(num_layers+1), context)
 
-        self.attention_block = AttentionBlock(num_filters*9, num_filters*9)
+        self.attention_block = AttentionBlock(num_filters*(num_layers+1), num_filters*(num_layers+1))
         # self.attention_block = AttentionBlockSparse(num_filters*9, num_filters*9)
 
-        self.G = G(attention_fn, dec_filter_size, num_filters)
+        self.G = G(attention_fn, dec_filter_size, num_filters, num_layers, is_skip_attention, context)
+        self.conv2 = conv(dec_filter_size, num_filters, 1, context)
 
-        self.conv2 = conv(dec_filter_size, num_filters, 1)
-
-    def forward(self, x, is_sep=True):
-
-        if is_sep:
-
-            # encoder
-
-            x, skip_layers = self.F(x)
-            x = self.conv1(x)
-
-            # attention_module
-
-            s1_emb, s2_emb, beta1, beta2 = self.attention_block(x)
-
-            # decoder
-
-            s1, betas1 = self.G(s1_emb, skip_layers, is_sep)
-            s1 = self.conv2(s1)
-
-            s2, betas2 = self.G(s2_emb, skip_layers, is_sep)
-            s2 = self.conv2(s2)
-
-            return s1, s2, [beta1] + betas1, [beta2] + betas2
-
-        else:
-            x, skip_layers = self.F(x)
-
-            x = self.conv1(x)
-
-            recon, _ = self.G(x, skip_layers, is_sep)
-            recon = self.conv2(recon)
-            return recon
-
-
-class F(nn.Module):
-    def __init__(self, enc_filter_size, num_filters):
-        super(F, self).__init__()
-
-        self.down1 = down(enc_filter_size, 1, num_filters)
-        self.down2 = down(enc_filter_size, num_filters, num_filters*2)
-        self.down3 = down(enc_filter_size, num_filters*2, num_filters*3)
-        self.down4 = down(enc_filter_size, num_filters*3, num_filters*4)
-        self.down5 = down(enc_filter_size, num_filters*4, num_filters*5)
-        self.down6 = down(enc_filter_size, num_filters*5, num_filters*6)
-        self.down7 = down(enc_filter_size, num_filters*6, num_filters*7)
-        self.down8 = down(enc_filter_size, num_filters*7, num_filters*8)
+        self.centercrop = CenterCrop()
 
     def forward(self, x):
 
-        x, skip1 = self.down1(x)
-        x, skip2 = self.down2(x)
-        x, skip3 = self.down3(x)
-        x, skip4 = self.down4(x)
-        x, skip5 = self.down5(x)
-        x, skip6 = self.down6(x)
-        x, skip7 = self.down7(x)
-        x, skip8 = self.down8(x)
+        # if is_sep:
 
-        return x, [skip1, skip2, skip3, skip4, skip5, skip6, skip7, skip8]
+        # encoder
+        mix = x
+        x, skip_layers = self.F(x)
+        x = self.conv1(x)
+
+        # attention_module
+
+        s1_emb, s2_emb, beta1, beta2 = self.attention_block(x)
+
+        # decoder
+
+        s1, betas1 = self.G(s1_emb, skip_layers)
+        # s1 = self.conv2(s1)
+
+        if self.is_diff:
+
+            mix = self.centercrop(s1, mix)
+            s2 = mix - s1
+            # Todo : beta
+            betas2 = betas1
+        else:
+            s2, betas2 = self.G(s2_emb, skip_layers)
+
+        return (s1, s2), ([beta1] + betas1, [beta2] + betas2)
+
+
+class F(nn.Module):
+    def __init__(self, enc_filter_size, num_filters, num_layers, context):
+        super(F, self).__init__()
+
+        self.downs = nn.ModuleList()
+        self.downs.append(down(enc_filter_size, 1, num_filters, context=context))
+        self.downs.extend([down(enc_filter_size, num_filters*layer, num_filters*(layer+1), context=context)
+                           for layer in range(1, num_layers)])
+
+    def forward(self, x):
+
+        skips = []
+        skips.append(x)
+        for down in self.downs:
+            x, skip = down(x)
+            skips.append(skip)
+        return x, skips
 
 
 class G(nn.Module):
-    def __init__(self, attention_fn, dec_filter_size, num_filters):
+    def __init__(self, attention_fn, dec_filter_size, num_filters, num_layers, is_skip_attention=True, context=True):
         super(G, self).__init__()
 
-        self.up1 = UpSkip(attention_fn, dec_filter_size, num_filters*9, num_filters*8)
-        self.up2 = UpSkip(attention_fn, dec_filter_size, num_filters*8, num_filters*7)
-        self.up3 = UpSkip(attention_fn, dec_filter_size, num_filters*7, num_filters*6)
-        self.up4 = UpSkip(attention_fn, dec_filter_size, num_filters*6, num_filters*5)
-        self.up5 = UpSkip(attention_fn, dec_filter_size, num_filters*5, num_filters*4)
-        self.up6 = UpSkip(attention_fn, dec_filter_size, num_filters*4, num_filters*3)
-        self.up7 = UpSkip(attention_fn, dec_filter_size, num_filters*3, num_filters*2)
-        self.up8 = UpSkip(attention_fn, dec_filter_size, num_filters*2, num_filters)
+        self.is_skip_attention = is_skip_attention
+        self.ups = nn.ModuleList(
+            [UpSkip(attention_fn, dec_filter_size, num_filters*(layer+2), num_filters*(layer+1), is_skip_attention, context)
+             for layer in reversed(range(num_layers))]
+        )
 
-    def forward(self, x, skip_layers, is_sep=True):
+        self.output = OutputLayer(attention_fn, dec_filter_size, num_filters, 1)
 
-        o, beta1 = self.up1(x, skip_layers[-1], is_sep)
-        o, beta2 = self.up2(o, skip_layers[-2], is_sep)
-        o, beta3 = self.up3(o, skip_layers[-3], is_sep)
-        o, beta4 = self.up4(o, skip_layers[-4], is_sep)
-        o, beta5 = self.up5(o, skip_layers[-5], is_sep)
-        o, beta6 = self.up6(o, skip_layers[-6], is_sep)
-        o, beta7 = self.up7(o, skip_layers[-7], is_sep)
-        o, beta8 = self.up8(o, skip_layers[-8], is_sep)
+    def forward(self, x, skip_layers):
 
-        return o, [beta1, beta2, beta3, beta4, beta5, beta6, beta7, beta8]
+        betas = []
+        for i, up in enumerate(self.ups):
+            x, beta = up(x, skip_layers[-i-1])
+            betas.append(beta)
+
+        x, beta = self.output(x, skip_layers[0], False)
+        betas.append(beta)
+
+        return x, betas
 
 
 class CrossAttention(nn.Module):
@@ -161,49 +154,127 @@ class AttentionBlockSparse(nn.Module):
         emb1 = torch.mul(x, beta[:,:,:,0])
         emb2 = torch.mul(x, beta[:,:,:,1])
 
-        return emb1, emb2, beta
+        return emb1, emb2, beta[:,:,:,0], beta[:,:,:,1]
 
 
 class SelfAttention(nn.Module):
     def __init__(self, in_ch, out_ch):
         super(SelfAttention, self).__init__()
 
-        self.conv = nn.Conv1d(out_ch, out_ch, 1)
-        self.activation = nn.Sigmoid()
+        self.conv1 = nn.Conv1d(out_ch, out_ch, 15, padding=7)
+        self.conv2 = nn.Conv1d(out_ch, out_ch, 15, padding=7)
+        self.conv3 = nn.Conv1d(out_ch, out_ch, 15, padding=7)
+        self.conv4 = nn.Conv1d(out_ch, out_ch, 15, padding=7)
+
+        self.activation = nn.LeakyReLU(negative_slope=0.2)
+        self.map = nn.Sigmoid()
 
     def forward(self, x, skip=None, before_act=False):
 
         if skip is not None:
             x = skip
 
-        feature = self.conv(x)
+        x = self.conv1(x)
+        x = self.activation(x)
+
+        x = self.conv2(x)
+        x = self.activation(x)
+
+        x = self.conv3(x)
+        x = self.activation(x)
+
+        feature = self.conv4(x)
 
         if before_act:
             return feature
 
-        beta = self.activation(feature)
+        beta = self.map(feature)
         x = torch.mul(beta, x)
 
         return x, beta
 
 
-class UpSkip(nn.Module):
+class OutputLayer(nn.Module):
     def __init__(self, attention_fn, dec_filter_size, emb_ch, skip_ch):
+        super(OutputLayer, self).__init__()
+
+        self.attention_fn = SelfAttention(emb_ch, skip_ch) if attention_fn == 'self_attention' \
+            else CrossAttention(emb_ch, skip_ch)
+
+        self.conv = nn.Conv1d(emb_ch+skip_ch, skip_ch, dec_filter_size, padding=dec_filter_size//2)
+        self.centercrop = CenterCrop()
+
+    def forward(self, x, skip, is_sep=True):
+
+        beta = None
+        if is_sep:
+            skip, beta = self.attention_fn(x, skip)
+
+        skip = self.centercrop(x, skip)
+        x = torch.cat((skip, x), 1) # channel-wise
+
+        x = self.conv(x)
+
+        return x, beta
+
+
+class Interpolate(nn.Module):
+    def __init__(self, context):
+        super(Interpolate, self).__init__()
+
+        self.context = context
+
+    def forward(self, x):
+
+        if self.context:
+            length = x.size(-1)
+            return f.interpolate(x, size=2*length-1, mode='linear', align_corners=True)
+
+        else:
+            return f.interpolate(x, scale_factor=2, mode='linear', align_corners=True)
+
+
+class CenterCrop(nn.Module):
+    def __init__(self):
+        super(CenterCrop, self).__init__()
+
+    def forward(self, x, skip):
+
+        x_length = x.size(-1)
+        skip_length = skip.size(-1)
+
+        diff = (skip_length - x_length)//2
+        assert(diff>=0)
+
+        return skip[:, :, diff: x_length + diff]
+
+
+class UpSkip(nn.Module):
+    def __init__(self, attention_fn, dec_filter_size, emb_ch, skip_ch, is_skip_attention, context):
         super(UpSkip, self).__init__()
 
         self.attention_fn = SelfAttention(emb_ch, skip_ch) if attention_fn == 'self_attention' \
             else CrossAttention(emb_ch, skip_ch)
 
-        self.upsample = nn.Upsample(scale_factor=2, mode='linear', align_corners=True)
-        self.conv = nn.Conv1d(emb_ch+skip_ch, skip_ch, dec_filter_size, padding=dec_filter_size//2)
+        self.is_skip_attention = is_skip_attention
+        self.context = context
+
+        self.upsample = Interpolate(self.context)
+        self.centercrop = CenterCrop()
+
+        self.conv = nn.Conv1d(emb_ch+skip_ch, skip_ch, dec_filter_size, padding=0) if context \
+            else nn.Conv1d(emb_ch+skip_ch, skip_ch, dec_filter_size, padding=dec_filter_size//2)
+
         self.activation = nn.LeakyReLU(negative_slope=0.2)
 
-    def forward(self, x, skip, is_sep=True):
+    def forward(self, x, skip):
 
         x = self.upsample(x)
 
         beta = None
-        if is_sep:
+        skip = self.centercrop(x, skip)
+
+        if self.is_skip_attention:
             skip, beta = self.attention_fn(x, skip)
 
         x = torch.cat((skip, x), 1) # channel-wise
@@ -232,13 +303,14 @@ class up(nn.Module):
 
 
 class down(nn.Module):
-    def __init__(self, enc_filter_size, in_ch, out_ch):
+    def __init__(self, enc_filter_size, in_ch, out_ch, context):
         super(down, self).__init__()
 
-        self.conv = nn.Conv1d(in_ch, out_ch, enc_filter_size, padding=enc_filter_size//2)
-        self.activation = nn.LeakyReLU(negative_slope=0.2)
+        self.conv = nn.Conv1d(in_ch, out_ch, enc_filter_size, padding=0) if context \
+            else nn.Conv1d(in_ch, out_ch, enc_filter_size, padding=enc_filter_size//2)
 
-        self.pooling = nn.MaxPool1d(2)
+        self.activation = nn.LeakyReLU(negative_slope=0.2)
+        # self.pooling = nn.MaxPool1d(2)
 
     def forward(self, x):
 
@@ -247,16 +319,19 @@ class down(nn.Module):
 
         skip = x
 
-        x = self.pooling(x)
+        # x = self.pooling(x)
+        # decimate : 1108
+        x = x[:, :, ::2]
 
         return x, skip
 
 
 class conv(nn.Module):
-    def __init__(self, enc_filter_size, in_ch, out_ch):
+    def __init__(self, enc_filter_size, in_ch, out_ch, context):
         super(conv, self).__init__()
 
-        self.conv = nn.Conv1d(in_ch, out_ch, enc_filter_size, padding=enc_filter_size//2)
+        self.conv = nn.Conv1d(in_ch, out_ch, enc_filter_size, padding=0) if context \
+            else nn.Conv1d(in_ch, out_ch, enc_filter_size, padding=enc_filter_size//2)
         self.activation = nn.LeakyReLU(negative_slope=0.2)
 
     def forward(self, x):
@@ -268,9 +343,12 @@ class conv(nn.Module):
 
 if __name__ == '__main__':
     # (bs, c, t)
-    attnnet = AttnNet(attention_fn='cross_attention', num_layers=8, num_filters=24, enc_filter_size=15,
-                      dec_filter_size=5)
+    attnnet = AttnNet(attention_fn='cross_attention', num_layers=12, num_filters=24, enc_filter_size=15,
+                      dec_filter_size=5, context=True)
 
-    x = torch.ones((1, 1, 16384))
-    s1, s2, betas1, betas2 = attnnet(x)
+    # x = torch.ones((1, 1, 16384))
+    # x = torch.ones((1, 1, 24563)) # num_layer=8
+    x = torch.ones((1, 1, 147443)) # num_layer=12
+
+    (s1, s2), (betas1, betas2) = attnnet(x)
     print(s1.size(), s2.size())
