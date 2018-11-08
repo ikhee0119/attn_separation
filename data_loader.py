46dbdbc44a60de6e5bf4f21cebede3702e0bd926
ikhee0119 <ikhee0119@gmail.com>
2018-11-08 13:45:28 +0900

    add context case for loader

--- data_loader.py
+++ data_loader.py
@@ -8,11 +8,16 @@ import Utils
 
 class Dataset(data.Dataset):
 
-    def __init__(self, dataset_path, tracks, input_length, is_augment=True):
+    def __init__(self, dataset_path, tracks, input_length, output_length, is_augment=True):
 
         self.dataset_path = os.path.join(dataset_path, 'train')
         self.tracks = tracks
+
         self.input_length = input_length
+        self.output_length = output_length
+
+        assert((self.input_length - self.output_length) % 2 == 0)
+        self.pad = int((self.input_length - self.output_length)/2)
 
         self.is_augment = is_augment
         self.load_files()
@@ -35,26 +40,31 @@ class Dataset(data.Dataset):
         mix, accompany, vocal = self.sequences[track]
         length = accompany.shape[1]
 
+        mix = np.pad(mix, ((0, 0), (self.pad, self.pad)), 'constant', constant_values=((0, 0), (0, 0)))
+        accompany = np.pad(accompany, ((0, 0), (self.pad, self.pad)), 'constant', constant_values=((0, 0), (0, 0)))
+        vocal = np.pad(vocal, ((0, 0), (self.pad, self.pad)), 'constant', constant_values=((0, 0), (0, 0)))
+
         start_idx = np.squeeze(np.random.randint(0, length - self.input_length +1, 1))
         end_idx = start_idx + self.input_length
 
-        mix = mix[:, start_idx:end_idx]
+        mix = mix[:, start_idx: end_idx]
         accompany = accompany[:, start_idx:end_idx]
         vocal = vocal[:, start_idx:end_idx]
 
         if self.is_augment:
             accompany *= np.random.uniform(0.7, 1.0)
             vocal *= np.random.uniform(0.7, 1.0)
+
             mix = accompany + vocal
 
-        return mix, accompany, vocal
+        return mix, accompany[:, self.pad:-self.pad], vocal[:, self.pad:-self.pad]
 
     def __len__(self):
         """Return the number of tracks."""
         return len(self.sequences)
 
 
-def get_loader(dataset_path, input_length, batch_size, num_workers=2):
+def get_loader(dataset_path, batch_size, input_length, output_length, num_workers=2):
 
     # to fix valid set
     np.random.seed(1337)
@@ -75,8 +85,8 @@ def get_loader(dataset_path, input_length, batch_size, num_workers=2):
     train_tracks = tracks[:75]
     valid_tracks = tracks[75:]
 
-    train_dataset = Dataset(dataset_path, train_tracks, input_length)
-    valid_dataset = Dataset(dataset_path, valid_tracks, input_length)
+    train_dataset = Dataset(dataset_path, train_tracks, input_length, output_length)
+    valid_dataset = Dataset(dataset_path, valid_tracks, input_length, output_length)
 
     train_loader = data.DataLoader(dataset=train_dataset,
                                    batch_size=batch_size,
@@ -102,7 +112,8 @@ if __name__ == '__main__':
     """
     train_loader, _ = get_loader(
         dataset_path='../dataset/musdb18',
-        input_length=16384,
+        input_length=24563,
+        output_length=16389,
         batch_size=2
     )
 
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 from data_loader import get_loader
 from Trainer import Trainer
 from Tester import Tester
+from Utils import compute_input_length
 
 os.environ["CUDA_VISIBLE_DEVICES"]="1"
 
@@ -23,8 +24,17 @@ def train(config):
     else:
         os.makedirs(exp_path)
 
-    train_loader, valid_loader = get_loader(config.data_path, config.input_length,
-                                            config.batch_size)
+    if config.context:
+        # for context mode, previous and future information is needed.
+
+        input_length, output_length = compute_input_length(config.input_length, config.num_layers,
+                                                           config.enc_filter_size, config.dec_filter_size)
+
+        config.input_length = input_length
+        config.output_length = output_length
+
+    train_loader, valid_loader = get_loader(config.data_path, config.batch_size,
+                                            config.input_length, config.output_length)
 
     trainer = Trainer(train_loader, valid_loader, config)
     trainer.train()
@@ -36,8 +46,18 @@ def test(config):
     exp_path = os.path.join(config.log_path, exp_name)
     config.exp_path = exp_path
 
-    tester = Tester(config.target_wav, config)
+    tester = Tester(config, config.target_wav)
     tester.predict()
+
+
+def evaluate(config):
+
+    exp_name = config.model_name + config.exp_number
+    exp_path = os.path.join(config.log_path, exp_name)
+    config.exp_path = exp_path
+
+    tester = Tester(config, None)
+    tester.produce_musdb_source_estimates()
 
 if __name__ == '__main__':
 
@@ -45,19 +65,21 @@ if __name__ == '__main__':
 
     # model name
 
-    parser.add_argument('--mode', choices=['train', 'test'], default='train')
+    parser.add_argument('--mode', choices=['train', 'test', 'eval'], default='train')
     parser.add_argument('--data_path', default='../dataset/musdb18')
     parser.add_argument('--log_path', default='../log/attn_separation')
 
     parser.add_argument('--model_name', choices=['attention'], default='attention')
-    parser.add_argument('--exp_number', default='1')
+    parser.add_argument('--exp_number', default='10')
 
     # architecture parameters
 
     parser.add_argument('--attention_fn', choices=['self_attention, cross_attention'], default='cross_attention')
 
     parser.add_argument('--num_sources', type=int, default=2)
+    parser.add_argument('--context', type=bool, default=True)
     parser.add_argument('--input_length', type=int, default=16384)
+    parser.add_argument('--output_length', type=int, default=16384)
 
     parser.add_argument('--num_layers', type=int, default=8)
     parser.add_argument('--num_filters', type=int, default=24)
@@ -72,9 +94,9 @@ if __name__ == '__main__':
     parser.add_argument('--use_tensorboard', type=bool, default=True)
 
     parser.add_argument('--log_step', type=int, default=1)
-    parser.add_argument('--model_save_step', type=int, default=2000)
+    parser.add_argument('--model_save_step', type=int, default=1000)
     parser.add_argument('--lr_update_step', type=int, default=1000)
-    parser.add_argument('--valid_step', type=int, default=10)
+    parser.add_argument('--valid_step', type=int, default=1000)
 
     parser.add_argument('--num_iters', type=int, default=200000)
 
@@ -82,9 +104,12 @@ if __name__ == '__main__':
 
     # test setting
 
-    parser.add_argument('--saved_model', default='12000-AttnNet.ckpt')
+    parser.add_argument('--saved_model', default='132000-AttnNet.ckpt')
+    parser.add_argument('--target_wav', default="../example/mallory.wav")
     # parser.add_argument('--target_wav', default="../example/Angels In Amplifiers - I'm Alright.stem_mix.wav")
-    parser.add_argument('--target_wav', default="../example/The Mountaineering Club - Mallory.stem_mix.wav")
+    # parser.add_argument('--target_wav', default="../example/The Mountaineering Club - Mallory.stem_mix.wav")
+    # parser.add_argument('--target_wav', default="../example/mallory_acc.wav")
+    # parser.add_argument('--target_wav', default="../example/jamesmay_ifyousay_mix.wav")
 
     config = parser.parse_args()
 
@@ -93,3 +118,6 @@ if __name__ == '__main__':
 
     elif config.mode == 'test':
         test(config)
+
+    elif config.mode == 'eval':
+        evaluate(config)
